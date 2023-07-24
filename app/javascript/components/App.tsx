e56af562c35b4bacea8e33fe01df799e9753b748
edwinbbu <edwinbbu@gmail.com>
2023-07-24 08:44:39 +0530

    Basic Hero page

--- app/javascript/components/App.tsx
+++ app/javascript/components/App.tsx
@@ -1,9 +1,6 @@
+import { LandingPage } from "../pages/Landing";
 import React from "react";
 
 export const App = () => {
-  return (
-    <div className="text-5xl text-blue-600">
-      React with ts, vite and tailwindcss.
-    </div>
-  );
+  return <LandingPage />;
 };
