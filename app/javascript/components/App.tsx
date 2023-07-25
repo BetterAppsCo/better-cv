b77bc265e6f24dc4495763df1a6480f995043da7
edwinbbu <edwinbbu@gmail.com>
2023-07-25 08:21:10 +0530

    All routes to react application

--- app/javascript/components/App.tsx
+++ app/javascript/components/App.tsx
@@ -1,6 +1,15 @@
 import { LandingPage } from "../pages/Landing";
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Home } from "./Home";
 
 export const App = () => {
-  return <LandingPage />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/app" Component={Home} />
+        <Route path="*" Component={LandingPage} />
+      </Routes>
+    </Router>
+  );
 };
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
-  root 'home#index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
   draw :api
+  
+  root 'home#index'
+  get '*path', to: 'home#index', via: :all
 end
