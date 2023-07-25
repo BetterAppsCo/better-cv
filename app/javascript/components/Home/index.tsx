f44b86d735654c6e75881b993ad992db64a9b7b2
edwinbbu <edwinbbu@gmail.com>
2023-07-25 08:45:09 +0530

    Nav changes

--- app/javascript/components/Home/index.tsx
+++ app/javascript/components/Home/index.tsx
@@ -31,8 +31,8 @@ const teams = [
   { id: 3, name: "Workcation", href: "#", initial: "W", current: false },
 ];
 const userNavigation = [
-  { name: "Your profile", href: "#" },
-  { name: "Sign out", href: "#" },
+  { name: "Your profile", href: "/profile" },
+  { name: "Sign out", href: "/" },
 ];
 
 function classNames(...classes) {
