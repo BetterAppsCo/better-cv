1c6c3342687e6e5aa01a03a4790528cbe3aed07e
edwinbbu <edwinbbu@gmail.com>
2023-07-25 08:32:32 +0530

    Home page added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,18 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
   content: [
-    './public/*.html',
-    './app/helpers/**/*.rb',
-    './app/javascript/**/*.{js,jsx,ts,tsx}',
-    './app/views/**/*.{erb,haml,html,slim}'
+    "./public/*.html",
+    "./app/helpers/**/*.rb",
+    "./app/javascript/**/*.{js,jsx,ts,tsx}",
+    "./app/views/**/*.{erb,haml,html,slim}",
   ],
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
       },
     },
   },
-  plugins: []
-}
+  plugins: [require("@tailwindcss/forms"), require("@tailwindcss/typography")],
+};
