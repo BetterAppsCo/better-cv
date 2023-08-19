4ff07183d915119bef315b266fc3ad7b262c2dc9
edwinbbu <edwinbbu@gmail.com>
2023-08-19 23:00:25 +0530

    User details displayed using mui

--- app/javascript/components/Profile/index.tsx
+++ app/javascript/components/Profile/index.tsx
@@ -1,12 +1,31 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Layout } from "../Layout";
 import { getCurrentUser } from "../../actions/profile";
+import Typography from "@mui/joy/Typography";
 
 export const Profile = () => {
-  useEffect(async () => {
-    const user = await getCurrentUser();
-    console.log("user:", user);
+  const [user, setUser] = useState({
+    name: "",
+    email: "",
+  });
+  const getUser = async () => {
+    try {
+      const user = await getCurrentUser();
+      setUser(user);
+    } catch (err) {
+      console.log("Error on fetching user", err);
+    }
+  };
+
+  useEffect(() => {
+    getUser();
   }, []);
 
-  return <Layout>Profile</Layout>;
+  return (
+    <Layout>
+      <Typography level="h2">Profile</Typography>
+      <Typography>{user.name}</Typography>
+      <Typography>{user.email}</Typography>
+    </Layout>
+  );
 };
