6aa786c2523e8b763020048d972884286dd3554e
edwinbbu <edwinbbu@gmail.com>
2023-09-18 11:44:08 +0530

    Scss preprocessor added

--- app/javascript/components/Dashboard/index.tsx
+++ app/javascript/components/Dashboard/index.tsx
@@ -1,8 +1,24 @@
 import React from "react";
 import { Layout } from "../Layout";
+import Input from "@mui/joy/Input";
+import Textarea from "@mui/joy/Textarea";
 
 type Props = {};
 
 export const Dashboard = (props: Props) => {
-  return <Layout>Dashboard</Layout>;
+  return (
+    <Layout>
+      Profile
+      <div className="max-w-xl space-y-2">
+        <Input placeholder="Name" />
+        <Input placeholder="Email" />
+        <Textarea
+          placeholder="Address"
+          minRows={4}
+          variant="outlined"
+          color="neutral"
+        />
+      </div>
+    </Layout>
+  );
 };
